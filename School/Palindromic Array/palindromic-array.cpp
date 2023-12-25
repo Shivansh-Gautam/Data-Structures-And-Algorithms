bb116a6a6ff283c5594034047d22cbb46523966f
Shivansh-Gautam <141754929+Shivansh-Gautam@users.noreply.github.com>
2023-12-25 15:14:58 +0530

    Added Solution - GfG to GitHub

---
+++ School/Palindromic Array/palindromic-array.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*Complete the function below*/
+
+class Solution {
+public:
+    int reverseDigits(int num) 
+{ 
+    int rev_num = 0; 
+    while (num > 0) { 
+        rev_num = rev_num * 10 + num % 10; 
+        num = num / 10; 
+    } 
+    return rev_num; 
+} 
+    int PalinArray(int a[], int n)
+    {
+    	// code here
+    	for(int i=0;i<n;i++){
+    	   int temp = reverseDigits(a[i]);
+    	   if(temp!=a[i]) return 0;
+    	}
+    	return 1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i = 0; i < n; i++)
+			cin>>a[i];
+		Solution obj;
+		cout<<obj.PalinArray(a,n)<<endl;
+	}
+}
+// } Driver Code Ends
