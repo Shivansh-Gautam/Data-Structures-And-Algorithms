36a2ce89cf4c9db51b12f9c2f62611c2e10580ec
Shivansh Gautam <141754929+Shivansh-Gautam@users.noreply.github.com>
2024-01-29 17:07:34 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count digit groupings of a number/count-digit-groupings-of-a-number.cpp
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+	int TotalCount(string str){
+	    // Code here
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string str;
+		cin >> str;
+		Solution ob;
+		int ans = ob.TotalCount(str);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
