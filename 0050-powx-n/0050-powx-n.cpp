bf125137bef007376a8dfb98ef280d2a1fe8c0ec
Shivansh Gautam <141754929+Shivansh-Gautam@users.noreply.github.com>
2024-04-18 12:37:34 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (22.38%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (n == 0) return 1.0;
-
-    double result = myPow(x, n / 2);
-    result *= result;
-
-    if (n % 2 != 0) {
-        if (n > 0) {
-            result *= x;
-        } else {
-            result /= x;
-        }
-    }
-
-    return result;
+        return pow(x,n);
     }
 };
