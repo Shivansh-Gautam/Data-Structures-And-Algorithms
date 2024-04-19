c5278822871ed938694e4afbfdb6e55e4c6e1699
Shivansh Gautam <141754929+Shivansh-Gautam@users.noreply.github.com>
2024-04-19 13:27:13 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find missing in second array/find-missing-in-second-array.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	vector<int> findMissing(int a[], int b[], int n, int m) 
+	{ 
+	    // Your code goes here
+	      unordered_map<int, int> mp;
+        int x=0, i=0, count=0;
+        vector<int> rs;
+        
+        for(int i=0; i<m; i++)  mp[b[i]]++;
+        
+        for(i=0; i<n; i++)
+        {
+            int num = a[i];
+            if(mp.find(num) == mp.end())    rs.push_back(num);
+        }
+        return rs;
+	} 
+};
+	
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+        cin>>n;
+        int m;
+        cin>>m;
+        int a[n];
+        int b[m];
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+        for(int i=0;i<m;i++)
+        cin >> b[i];
+        vector<int> ans;
+        
+
+        Solution ob;
+        ans=ob.findMissing(a,b,n,m);
+        for(int i=0;i<ans.size();i++)
+        cout<<ans[i]<<" ";
+	    
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+// } Driver Code Ends
