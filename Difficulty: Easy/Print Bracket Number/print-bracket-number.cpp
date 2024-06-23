0b430e1dc0cdf2378d6e99d22597d071cfb9c4b2
Shivansh Gautam <141754929+Shivansh-Gautam@users.noreply.github.com>
2024-06-23 22:18:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Bracket Number/print-bracket-number.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    vector<int> bracketNumbers(string str) {
+        // Your code goes here
+            vector<int> ans;
+        stack<pair<char,int>> st;
+        int cnt=0;
+        for(char ch:str){
+            if(ch=='('){
+                st.push({ch,++cnt});
+                ans.push_back(cnt);
+            }
+            else if(ch==')'){
+                ans.push_back(st.top().second);
+                st.pop();
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+
+        Solution ob;
+
+        vector<int> ans = ob.bracketNumbers(s);
+
+        for (auto i : ans)
+            cout << i << " ";
+
+        cout << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
