39f427af39d469e66c9ebaa6b345a635e766b2f9
Shivansh Gautam <141754929+Shivansh-Gautam@users.noreply.github.com>
2024-08-12 15:02:00 +0530

    Time: 90 ms (70.09%) | Memory: 70.3 MB (59.30%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int target) {
+        int i = 0, j = 0, n = nums.size(), odds = 0, totalSubarrays = 0;
+
+        while (j < n) {
+          
+            if (nums[j] & 1) {
+                odds++;
+            }    
+
+           
+            while (i <= j && odds > target) {
+                if (nums[i] & 1) {
+                    odds--;
+                }
+                i++;
+            }
+
+          
+            totalSubarrays += j - i + 1;
+            j++;
+        }
+        return totalSubarrays;
+    }
+
+   
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return solve(nums, k) - solve(nums, k - 1);
+    }
+};
