f65423843ea3d9dfe33e50379d84aec2416318e3
Shivansh Gautam <141754929+Shivansh-Gautam@users.noreply.github.com>
2025-03-02 22:46:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -1,54 +1,71 @@
 //{ Driver Code Starts
-// Initial Template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-// User function template for C++
 
 class Solution {
   public:
-    // Function to find maximum of each subarray of size k.
-    vector<int> max_of_subarrays(int k, vector<int> &arr) {
-   vector<int>ans;
-        deque<int>dq;
-        for(int i=0;i<arr.size();i++){
-            if(dq.size() and dq.front()==i-k) dq.pop_front();
-            while(dq.size() and arr[dq.back()]<arr[i]) dq.pop_back();
+     vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        deque<int> dq;  // Deque to store indices of the array elements
+        int n = arr.size();
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++) {
+            // Remove elements from the back of the deque if they are smaller than the current element
+            while (!dq.empty() && arr[dq.back()] < arr[i]) {
+                dq.pop_back();
+            }
+            
+            // Add the current element index to the deque
             dq.push_back(i);
-            if(i>=k-1)ans.push_back(arr[dq.front()]);
+            
+            // Remove elements from the front of the deque if they are out of the current window
+            if (dq.front() <= i - k) {
+                dq.pop_front();
+            }
+            
+            // If the window has reached size k, add the current maximum (which is at the front of the deque)
+            if (i >= k - 1) {
+                ans.push_back(arr[dq.front()]);
+            }
         }
+        
         return ans;
     }
 };
 
+
 //{ Driver Code Starts.
 
 int main() {
-
     int t;
     cin >> t;
-    cin.ignore();
+    cin.ignore(); // Ignore newline character after t
 
     while (t--) {
-
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
         vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
+        int k;
+        string inputLine;
+
+        getline(cin, inputLine); // Read the array input as a line
+        stringstream ss(inputLine);
+        int value;
+        while (ss >> value) {
+            arr.push_back(value);
         }
+
+        cin >> k;
+        cin.ignore(); // Ignore newline character after k input
+
         Solution obj;
-        vector<int> res = obj.max_of_subarrays(k, arr);
+        vector<int> res = obj.maxOfSubarrays(arr, k);
         for (int i = 0; i < res.size(); i++)
             cout << res[i] << " ";
         cout << endl;
+        cout << "~"
+             << "\n";
     }
 
     return 0;
