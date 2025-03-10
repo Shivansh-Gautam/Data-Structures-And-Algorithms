da2711e0172a0d9f176f24b043f105d6f0aa3c19
Shivansh Gautam <141754929+Shivansh-Gautam@users.noreply.github.com>
2025-03-10 21:33:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.cpp
+++ Difficulty: Hard/Edit Distance/edit-distance.cpp
@@ -1,58 +1,68 @@
 //{ Driver Code Starts
+// Initial Template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    int editDistance(string str1, string str2) {
-        // Code here
-        int m = str1.size();
-        int n = str2.size();
+    // Function to compute the edit distance between two strings
+   int m , n;
+    int dp[1001][1001];
+    int solve(int i,int j,string &s,string &t) {
+        if(i >= m)
+           return n-j;
+        if(j >= n)
+           return m-i;
         
-        // Creating a 2D Vector DP 
-         vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        if(dp[i][j] != -1)
+            return dp[i][j];
         
-        // Iterating through words
-        for(int i = 0; i <= m; i++){
-            for(int j = 0; j <= n; j++){
-                // If i == 0, i.e STR1 is empty, meaning it will require j number of insertions
-                if(i == 0){
-                    dp[i][j] = j;
-                }
-                
-                // If j == 0; i.e STR2 is empty, meaning it will require i number of deletions
-                else if(j == 0){
-                    dp[i][j] = i;
-                }
-                
-                // We got the same word, so we'll do nothing and take back the result of previous
-                else if(str1[i-1] == str2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                
-                // Word isn't matching so we'll take minimum of insertion, removal or replacement
-                else{
-                    dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
-                }
-            }
+        int opt = INT_MAX;
+        
+        if(s[i]==t[j]) {
+            return dp[i][j] = solve(i+1,j+1,s,t);
+        }   
+        else{
+            int op1 = 1 + solve(i,j+1,s,t);
+            int op2 = 1 + solve(i+1,j,s,t);
+            int op3 = 1 + solve(i+1,j+1,s,t);
+            
+            
+            opt = min(opt,min(op1,min(op2,op3)));
         }
         
-        return dp[m][n];
+        return dp[i][j] =  opt;
+    }
+    int editDistance(string& s1, string& s2) {
+        m = s1.size();
+        n = s2.size();
+        
+        memset(dp,-1,sizeof(dp));
+        
+        return solve(0,0,s1,s2);
     }
 };
 
+
 //{ Driver Code Starts.
+
 int main() {
+
     int T;
     cin >> T;
+    cin.ignore();
     while (T--) {
-        string s, t;
-        cin >> s >> t;
+        string s1;
+        getline(cin, s1);
+        string s2;
+        getline(cin, s2);
         Solution ob;
-        int ans = ob.editDistance(s, t);
+        int ans = ob.editDistance(s1, s2);
         cout << ans << "\n";
+        cout << "~" << endl;
     }
     return 0;
 }
